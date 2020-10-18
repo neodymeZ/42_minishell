9e6ca3e6fb371cd9eb9a5c756500f9acdf92e14c
gejeanet <gejeanet@student.21-school.ru>
2020-10-19 00:03:52 +0300

    ft_echo fix. Some testing were passed

--- srcs/builtin_ft_echo.c
+++ srcs/builtin_ft_echo.c
@@ -6,37 +6,47 @@
 /*   By: gejeanet <gejeanet@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 20:00:34 by gejeanet          #+#    #+#             */
-/*   Updated: 2020/10/18 22:53:56 by gejeanet         ###   ########.fr       */
+/*   Updated: 2020/10/19 00:01:29 by gejeanet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+** builtin "echo" implementation (with ONLY -n option)
+** ATTENTION! It is possible to pass to the "echo" command
+** multiple options which are the same.
+** For example:
+** echo -n -n -n -e -n
+** Another interesting example is:
+** echo -n -n 1234567 -n -n
+*/
+
 int				ft_echo(char **args)
 {
-	int		result;
 	int		lf_flag;
+	int		opts_end_flag;
 
-	result = 0;
 	lf_flag = 0;
+	opts_end_flag = 0;
 	if (args != NULL && *args != NULL)
 	{
-		if ((ft_strncmp(*args, "-n", 2) == 0) && (ft_strlen(*args) == 2))
-		{
-			lf_flag = 1;
-			args++;
-			continue ;
-		}
-		ft_putstr_fd(args, 1);
-		args++;
 		while (*args != NULL)
 		{
-			ft_putstr_fd(" ", 1);
-			ft_putstr_fd(args, 1);
+			if ((ft_strncmp(*args, "-n", 2) != 0) || (ft_strlen(*args) != 2) \
+				|| opts_end_flag == 1)
+			{
+				opts_end_flag = 1;
+				ft_putstr_fd(*args++, 1);
+				if (*args != NULL)
+					ft_putstr_fd(" ", 1);
+				continue ;
+			}
+			lf_flag = 1;
 			args++;
 		}
 	}
 	if (lf_flag == 0)
 		ft_putstr_fd("\n", 1);
-	return (result);
+	return (0);
 }
